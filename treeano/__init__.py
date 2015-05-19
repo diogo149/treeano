63babbae8f048385267e0e4074c5223daa77be29
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-05-18 23:22:38 -0700

    add additional imports

--- treeano/__init__.py
+++ treeano/__init__.py
@@ -5,6 +5,15 @@ lasagne
 import core
 import nodes
 
+from core import (UpdateDeltas,
+                  SharedInitialization,
+                  WeightInitialization,
+                  VariableWrapper,
+                  register_node,
+                  NodeImpl,
+                  WrapperNodeImpl,
+                  Wrapper1NodeImpl)
+
 # ############################# FIXME DEPRECATED #############################
 
 from initialization import (SharedInitialization,
