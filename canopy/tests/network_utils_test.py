3eb0b5c12e76028bb4971879480d19b372f919dd
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-06-12 19:57:00 -0700

    test that preallocated init shares shared

--- canopy/tests/network_utils_test.py
+++ canopy/tests/network_utils_test.py
@@ -26,7 +26,7 @@ def test_to_shared_dict():
                             42.42 * np.ones((10, 15), dtype=fX))
 
 
-def test_to_preallocated_init():
+def test_to_preallocated_init1():
     network1 = tn.SequentialNode(
         "seq",
         [tn.InputNode("i", shape=(3, 4, 5)),
@@ -55,3 +55,41 @@ def test_to_preallocated_init():
     x = np.random.randn(3, 4, 5).astype(fX)
     np.testing.assert_equal(fn1(x),
                             fn2(x))
+
+
+def test_to_preallocated_init2():
+    # test that networks are kept in sync even when updating
+    network1 = tn.toy.ConstantUpdaterNode(
+        "cun",
+        tn.SequentialNode(
+            "seq",
+            [tn.InputNode("i", shape=(3, 4, 5)),
+             tn.LinearMappingNode(
+                 "lm",
+                 output_dim=15,
+                 inits=[treeano.inits.NormalWeightInit(15.0)])]),
+        value=-0.1,
+    ).build()
+    inits = [canopy.network_utils.to_preallocated_init(network1)]
+    network2 = tn.toy.ConstantUpdaterNode(
+        "cun",
+        tn.SequentialNode(
+            "seq",
+            [tn.InputNode("i", shape=(3, 4, 5)),
+             tn.LinearMappingNode(
+                 "lm",
+                 output_dim=15,
+                 inits=inits)]),
+        value=0.4,
+    ).build()
+
+    fn1 = network1.function(["i"], ["lm"])
+    fn2 = network2.function(["i"], ["lm"])
+    fn1u = network1.function(["i"], ["lm"], include_updates=True)
+    fn2u = network2.function(["i"], ["lm"], include_updates=True)
+    x = np.random.randn(3, 4, 5).astype(fX)
+    np.testing.assert_equal(fn1(x), fn2(x))
+    fn1u(x)
+    np.testing.assert_equal(fn1(x), fn2(x))
+    fn2u(x)
+    np.testing.assert_equal(fn1(x), fn2(x))
