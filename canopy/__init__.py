5d063e954cb80535753cb8950e76fac852ac2c51
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-06-23 09:09:33 -0700

    add evaluate_until

--- canopy/__init__.py
+++ canopy/__init__.py
@@ -1,7 +1,9 @@
+import fn_utils
 import handlers
 import network_utils
 import node_utils
 import transforms
 import walk_utils
 
+from fn_utils import evaluate_until
 from handlers import handled_fn
---
+++ canopy/fn_utils.py
@@ -0,0 +1,15 @@
+import time
+
+
+def evaluate_until(fn, gen, max_iters=None, max_seconds=None):
+    """
+    evaluates a function on the output of a data generator until a given
+    stopping condition
+    """
+    start_time = time.time()
+    for i, data in enumerate(gen):
+        if max_iters is not None and i >= max_iters:
+            break
+        if max_seconds is not None and max_seconds >= time.time() - start_time:
+            break
+        fn(data)
