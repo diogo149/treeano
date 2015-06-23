e55736258f46362b4a0c2099417609af9e590060
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-06-23 09:44:01 -0700

    add default callback

--- canopy/fn_utils.py
+++ canopy/fn_utils.py
@@ -1,7 +1,18 @@
+from __future__ import division, absolute_import
+from __future__ import print_function, unicode_literals
+
 import time
 
 
-def evaluate_until(fn, gen, max_iters=None, max_seconds=None, callback=None):
+def _default_evaluate_until_callback(iter_num, res):
+    print("Iter(%d): %s" % (iter_num, res))
+
+
+def evaluate_until(fn,
+                   gen,
+                   max_iters=None,
+                   max_seconds=None,
+                   callback=_default_evaluate_until_callback):
     """
     evaluates a function on the output of a data generator until a given
     stopping condition
@@ -14,4 +25,4 @@ def evaluate_until(fn, gen, max_iters=None, max_seconds=None, callback=None):
             break
         res = fn(data)
         if callback is not None:
-            callback(res)
+            callback(i, res)
