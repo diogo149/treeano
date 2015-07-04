9322df968a2c12b98aeb5f070a5be419125e84ae
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-07-03 19:44:20 -0700

    remove unneeded comments

--- examples/discovering_hidden_factors_of_variation_in_deep_networks/mnist.py
+++ examples/discovering_hidden_factors_of_variation_in_deep_networks/mnist.py
@@ -84,11 +84,8 @@ model = tn.HyperparameterNode(
          tn.TotalCostNode(
              "cost",
              {"pred": tn.IdentityNode("recon_id"),
-                 # "pred": tn.ReferenceNode("recon_ref",
-              # reference="reconstruction"),
               "target": tn.ReferenceNode("in_ref", reference="x")},
              cost_function=treeano.utils.squared_error),
-         # TODO take out of toy module
          tn.toy.MultiplyConstantNode("mul_reconstruction_error", value=0.1),
          tn.InputElementwiseSumNode("total_cost")]),
     num_units=512,
