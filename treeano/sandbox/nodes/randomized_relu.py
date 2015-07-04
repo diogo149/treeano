8a764a9faf4f0e8a840e4b89aa226fa28b239985
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-07-04 00:50:38 -0700

    add randomized relu

---
+++ treeano/sandbox/nodes/randomized_relu.py
@@ -0,0 +1,48 @@
+"""
+from "Empirical Evaluation of Rectified Activations in Convolutional Network"
+http://arxiv.org/abs/1505.00853
+"""
+import toolz
+import numpy as np
+import theano
+import theano.tensor as T
+import treeano
+import treeano.nodes as tn
+from theano.sandbox.rng_mrg import MRG_RandomStreams
+
+
+@treeano.register_node("randomized_relu")
+class RandomizedReLUNode(treeano.NodeImpl):
+
+    hyperparameter_names = ("alpha_lower",
+                            "alpha_upper",
+                            "deterministic")
+
+    def compute_output(self, network, in_vw):
+        # gather hyperparameters
+        deterministic = network.find_hyperparameter(["deterministic"],
+                                                    False)
+        l = network.find_hyperparameter(["alpha_lower"],
+                                        3)
+        u = network.find_hyperparameter(["alpha_upper"],
+                                        8)
+
+        if deterministic:
+            negative_coefficient = 2.0 / (l + u)
+        else:
+            # TODO save this state so that we can seed the rng
+            srng = MRG_RandomStreams()
+            alphas = srng.uniform(size=in_vw.symbolic_shape(),
+                                  low=l,
+                                  high=u)
+            negative_coefficient = 1.0 / alphas
+
+        # return output
+        network.create_variable(
+            "default",
+            variable=treeano.utils.rectify(
+                in_vw.variable,
+                negative_coefficient=negative_coefficient),
+            shape=in_vw.shape,
+            tags={"output"},
+        )
---
+++ treeano/sandbox/nodes/tests/randimized_relu_test.py
@@ -0,0 +1,43 @@
+import nose.tools as nt
+import numpy as np
+import theano
+import theano.tensor as T
+
+import treeano
+import treeano.nodes as tn
+
+from treeano.sandbox.nodes import randomized_relu as rrelu
+
+
+fX = theano.config.floatX
+
+
+def test_rrelu_node_serialization():
+    tn.check_serialization(rrelu.RandomizedReLUNode("a"))
+
+
+def test_rrelu_node1():
+    network = tn.SequentialNode(
+        "s",
+        [tn.InputNode("i", shape=(1, 4)),
+         rrelu.RandomizedReLUNode("p", deterministic=True)]).network()
+
+    fn = network.function(["i"], ["p"])
+    x = np.array([[-1.0, -0.2, 0.2, 1.0]], dtype=fX)
+    ans = np.array([[-1.0 * 2 / 11, -0.2 * 2 / 11, 0.2, 1.0]], dtype=fX)
+    np.testing.assert_allclose(fn(x)[0],
+                               ans,
+                               rtol=1e-5)
+
+
+def test_rrelu_node2():
+    network = tn.SequentialNode(
+        "s",
+        [tn.InputNode("i", shape=(100, 100)),
+         rrelu.RandomizedReLUNode("p")]).network()
+
+    fn = network.function(["i"], ["p"])
+    x = -np.random.rand(100, 100).astype(fX)
+    res = fn(x)[0]
+    assert res.min() > -2 / 11
+    assert res.max() < 0
