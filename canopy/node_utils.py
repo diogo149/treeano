540cbc24e3a182f239c3a7504bc5121c1a0267cd
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-07-13 09:07:16 -0700

    add format node name to node_utils

--- canopy/node_utils.py
+++ canopy/node_utils.py
@@ -33,11 +33,33 @@ def suffix_node(root_node, suffix):
     def copy_and_suffix(node):
         assert node.name not in seen
         seen.add(node.name)
-        new_node = treeano.node_utils.copy_node(node)
         # assert that node is nodeimpl, since we only know how to set
         # name for those
-        assert isinstance(new_node, treeano.NodeImpl)
-        new_node._name += suffix
-        return new_node
+        assert isinstance(node, treeano.NodeImpl)
+        node._name += suffix
+        return node
 
-    return postwalk_node(root_node, copy_and_suffix)
+    # make a copy only once here
+    new_node = treeano.node_utils.copy_node(root_node)
+    return postwalk_node(new_node, copy_and_suffix)
+
+
+def format_node_name(root_node, format):
+    """
+    creates a copy of a node, with names suffixed by given suffix
+    """
+    # use seen set to make sure there are no bugs
+    seen = set()
+
+    def copy_and_format(node):
+        assert node.name not in seen
+        seen.add(node.name)
+        # assert that node is nodeimpl, since we only know how to set
+        # name for those
+        assert isinstance(node, treeano.NodeImpl)
+        node._name = format % node.name
+        return node
+
+    # make a copy only once here
+    new_node = treeano.node_utils.copy_node(root_node)
+    return postwalk_node(new_node, copy_and_format)
