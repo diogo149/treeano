7c93bb3d5f6e08408da9471b6d70f25208d90935
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-07-23 22:16:52 -0700

    fix whitespace

--- canopy/handlers/misc.py
+++ canopy/handlers/misc.py
@@ -2,16 +2,18 @@ from . import base
 
 
 class CallbackWithInput(base.NetworkHandlerImpl):
+
     """
-    handler that calls a callback with both the result of the function and the inputs to the function
+    handler that calls a callback with both the result of the function and the
+    inputs to the function
     """
 
     def __init__(self, callback):
         self.callback = callback
-        
+
     def call(self, fn, in_map, *args, **kwargs):
         res = fn(in_map, *args, **kwargs)
         self.callback(res, in_map)
         return res
-        
+
 callback_with_input = CallbackWithInput
