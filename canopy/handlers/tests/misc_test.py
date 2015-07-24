365b499d5e5b170ec9c4612abc7160225a6882f5
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-07-23 22:17:01 -0700

    add test for callback with input

---
+++ canopy/handlers/tests/misc_test.py
@@ -0,0 +1,30 @@
+import nose.tools as nt
+import numpy as np
+import theano
+import theano.tensor as T
+
+import treeano
+import treeano.nodes as tn
+import canopy
+
+
+fX = theano.config.floatX
+
+
+def test_callback_with_input():
+    vals = []
+
+    def save_sum(res_map, in_map):
+        vals.append(res_map["out"] + in_map["x"])
+
+    network = tn.InputNode("i", shape=()).network()
+    fn = canopy.handlers.handled_fn(
+        network,
+        [canopy.handlers.callback_with_input(save_sum)],
+        {"x": "i"},
+        {"out": "i"})
+    for i in range(100):
+        fn({"x": i})
+
+    np.testing.assert_equal(2 * np.arange(100),
+                            np.array(vals).ravel())
