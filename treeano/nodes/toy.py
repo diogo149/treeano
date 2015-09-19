060635fc78c7950d52568a1f5d13fdc55057d60b
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-09-19 00:20:32 -0700

    add scalar sum node

--- treeano/nodes/toy.py
+++ treeano/nodes/toy.py
@@ -19,3 +19,19 @@ class ConstantUpdaterNode(core.Wrapper1NodeImpl):
         value = network.find_hyperparameter(["value"])
         parameters = network.find_vws_in_subtree(tags=["parameter"])
         return core.UpdateDeltas({p.variable: value for p in parameters})
+
+
+@core.register_node("scalar_sum")
+class ScalarSumNode(core.NodeImpl):
+
+    """
+    sums up its input into a scalar
+    """
+
+    def compute_output(self, network, in_vw):
+        network.create_variable(
+            "default",
+            variable=in_vw.variable.sum(),
+            shape=(),
+            tags={"output"},
+        )
