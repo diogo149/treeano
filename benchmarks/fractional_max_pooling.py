f20aab24a664b9245ec81b3ee59953f145006521
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-09-24 23:19:05 -0700

    add fractional max pooling nodes

---
+++ benchmarks/fractional_max_pooling.py
@@ -0,0 +1,55 @@
+import numpy as np
+import theano
+import theano.tensor as T
+import treeano.nodes as tn
+from treeano.sandbox.nodes import fmp
+fX = theano.config.floatX
+
+# TODO change me
+node = "fmp2"
+compute_grad = True
+
+if node == "mp":
+    n = tn.MaxPool2DNode("mp", pool_size=(2, 2))
+elif node == "fmp":
+    n = fmp.DisjointPseudorandomFractionalMaxPool2DNode("fmp1",
+                                                        fmp_alpha=1.414,
+                                                        fmp_u=0.5)
+elif node == "fmp2":
+    n = fmp.OverlappingRandomFractionalMaxPool2DNode("fmp2",
+                                                     pool_size=(1.414, 1.414))
+else:
+    assert False
+
+
+network = tn.SequentialNode(
+    "s",
+    [tn.InputNode("i", shape=(1, 1, 32, 32)),
+     n]
+).network()
+
+
+if compute_grad:
+    i = network["i"].get_variable("default").variable
+    s = network["s"].get_variable("default").variable
+    fn = network.function(["i"], [T.grad(s.sum(), i)])
+else:
+    fn = network.function(["i"], ["s"])
+
+x = np.random.randn(1, 1, 32, 32).astype(fX)
+
+"""
+20150924 results:
+
+%timeit fn(x)
+
+no grad:
+mp: 33.7 us
+fmp: 77.6 us
+fmp2: 1.91 ms
+
+with grad:
+mp: 67.1 us
+fmp: 162 us
+fmp2: 2.66 ms
+"""
---
+++ treeano/sandbox/nodes/fmp.py
@@ -0,0 +1,113 @@
+"""
+from "Fractional Max-Pooling" http://arxiv.org/abs/1412.6071
+"""
+import numpy as np
+import theano
+import theano.tensor as T
+import treeano
+import treeano.nodes as tn
+import treeano.theano_extensions.fractional_max_pooling as fmp
+
+
+@treeano.register_node("disjoint_pseudorandom_fractional_max_pool_2d")
+class DisjointPseudorandomFractionalMaxPool2DNode(treeano.NodeImpl):
+
+    hyperparameter_names = ("fmp_alpha",
+                            "fmp_u")
+
+    def compute_output(self, network, in_vw):
+        assert in_vw.ndim == 4
+        alpha = network.find_hyperparameter(["fmp_alpha"])
+        u = network.find_hyperparameter(["fmp_u"])
+        op = fmp.DisjointPseudorandomFractionalMaxPooling2DOp(
+            alpha=alpha,
+            u=u
+        )
+        out_shape = list(in_vw.shape)
+        # currently must be the same height and width
+        assert out_shape[2] == out_shape[3]
+        if out_shape[2] is not None:
+            out_shape[2] = out_shape[3] = op.output_length(out_shape[2])
+        out_shape = tuple(out_shape)
+        network.create_variable(
+            "default",
+            variable=op(in_vw.variable),
+            shape=out_shape,
+            tags={"output"},
+        )
+
+
+@treeano.register_node("overlapping_random_fractional_max_pool_2d")
+class OverlappingRandomFractionalMaxPool2DNode(treeano.NodeImpl):
+
+    """
+    modified from https://github.com/Lasagne/Lasagne/pull/171/files
+    """
+
+    hyperparameter_names = ("pool_size",
+                            "pool_function")
+
+    def compute_output(self, network, in_vw):
+        if network.find_hyperparameter(["deterministic"]):
+            import warnings
+            warnings.warn("OverlappingRandomFractionalMaxPool2DNode has "
+                          "no deterministic implementation")
+
+        pool_size = network.find_hyperparameter(["pool_size"])
+        assert len(pool_size) == 2
+        for alpha in pool_size:
+            assert 1 < alpha < 2
+        pool_fn = network.find_hyperparameter(["pool_function"], T.max)
+
+        # NOTE: MRG_RandomStreams doesn't have "permutation"
+        srng = T.shared_randomstreams.RandomStreams()
+
+        def theano_shuffled(in_vw):
+            n = in_vw.shape[0]
+
+            shuffled = T.permute_row_elements(in_vw.T, srng.permutation(n=n)).T
+            return shuffled
+
+        out_shape = list(in_vw.shape)
+        for axis, alpha in zip([2, 3], pool_size):
+            out_shape[axis] = int(np.ceil(float(out_shape[axis]) / alpha))
+        out_shape = tuple(out_shape)
+
+        n_in0, n_in1 = in_vw.shape[2:]
+        n_out0, n_out1 = out_shape[2:]
+
+        # Variable stride across the input creates fractional reduction
+        a = theano.shared(
+            np.array([2] * (n_in0 - n_out0) + [1] * (2 * n_out0 - n_in0)))
+        b = theano.shared(
+            np.array([2] * (n_in1 - n_out1) + [1] * (2 * n_out1 - n_in1)))
+
+        # Randomize the input strides
+        a = theano_shuffled(a)
+        b = theano_shuffled(b)
+
+        # Convert to input positions, starting at 0
+        a = T.concatenate(([0], a[:-1]))
+        b = T.concatenate(([0], b[:-1]))
+        a = T.cumsum(a)
+        b = T.cumsum(b)
+
+        # Positions of the other corners
+        c = T.clip(a + 1, 0, n_in0 - 1)
+        d = T.clip(b + 1, 0, n_in1 - 1)
+
+        # Index the four positions in the pooling window and stack them
+        in_var = in_vw.variable
+        temp = T.stack(in_var[:, :, a, :][:, :, :, b],
+                       in_var[:, :, c, :][:, :, :, b],
+                       in_var[:, :, a, :][:, :, :, d],
+                       in_var[:, :, c, :][:, :, :, d])
+
+        out_var = pool_fn(temp, axis=0)
+
+        network.create_variable(
+            "default",
+            variable=out_var,
+            shape=out_shape,
+            tags={"output"}
+        )
