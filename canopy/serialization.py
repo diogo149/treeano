6dca50dedf68f60184bcecbbe5f54723872b6246
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-09-27 09:32:38 -0700

    py3: properly open pickle files in binary mode

--- canopy/serialization.py
+++ canopy/serialization.py
@@ -9,16 +9,16 @@ def pickle_network(network, dirname):
         os.mkdir(dirname)
     root_node = network.root_node
     value_dict = network_utils.to_value_dict(network)
-    with open(os.path.join(dirname, "root_node.pkl"), 'w') as f:
+    with open(os.path.join(dirname, "root_node.pkl"), 'wb') as f:
         pickle.dump(root_node, f, protocol=pickle.HIGHEST_PROTOCOL)
-    with open(os.path.join(dirname, "value_dict.pkl"), 'w') as f:
+    with open(os.path.join(dirname, "value_dict.pkl"), 'wb') as f:
         pickle.dump(value_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def unpickle_network(dirname):
-    with open(os.path.join(dirname, "root_node.pkl")) as f:
+    with open(os.path.join(dirname, "root_node.pkl"), 'rb') as f:
         root_node = pickle.load(f)
-    with open(os.path.join(dirname, "value_dict.pkl")) as f:
+    with open(os.path.join(dirname, "value_dict.pkl"), 'rb') as f:
         value_dict = pickle.load(f)
     network = root_node.network()
     network_utils.load_value_dict(network, value_dict)
