ad7a3d1fdae1f12341299bfd2e8375b83dc85177
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-09-27 00:46:51 -0700

    py3: more import changes

--- canopy/handlers/__init__.py
+++ canopy/handlers/__init__.py
@@ -1,30 +1,30 @@
-import base
-import conditional
-import nodes
-import batch
-import fn
-import monitor
-import debug
+from . import base
+from . import conditional
+from . import nodes
+from . import batch
+from . import fn
+from . import monitor
+from . import debug
 
-from base import (NetworkHandlerAPI,
-                  NetworkHandlerImpl)
-from fn import (handled_fn)
-from conditional import (call_after_every)
-from nodes import (with_hyperparameters,
-                   override_hyperparameters,
-                   update_hyperparameters,
-                   schedule_hyperparameter,
-                   use_scheduled_hyperparameter)
-from batch import (split_input,
-                   chunk_variables,
-                   batch_pad)
-from monitor import (time_call,
-                     time_per_row,
-                     evaluate_monitoring_variables,
-                     monitor_network_state)
-from misc import (callback_with_input,
-                  exponential_polyak_averaging)
-from debug import (output_nanguard,
-                   network_nanguard,
-                   nanguardmode,
-                   save_last_inputs_and_networks)
+from .base import (NetworkHandlerAPI,
+                   NetworkHandlerImpl)
+from .fn import (handled_fn)
+from .conditional import (call_after_every)
+from .nodes import (with_hyperparameters,
+                    override_hyperparameters,
+                    update_hyperparameters,
+                    schedule_hyperparameter,
+                    use_scheduled_hyperparameter)
+from .batch import (split_input,
+                    chunk_variables,
+                    batch_pad)
+from .monitor import (time_call,
+                      time_per_row,
+                      evaluate_monitoring_variables,
+                      monitor_network_state)
+from .misc import (callback_with_input,
+                   exponential_polyak_averaging)
+from .debug import (output_nanguard,
+                    network_nanguard,
+                    nanguardmode,
+                    save_last_inputs_and_networks)
