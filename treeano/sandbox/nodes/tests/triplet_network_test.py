d4e857c0c5282172cd51ace597274b746f36eb2f
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-09-29 22:45:27 -0700

    add triplet network op / support code

---
+++ treeano/sandbox/nodes/tests/triplet_network_test.py
@@ -0,0 +1,76 @@
+import numpy as np
+import theano
+import theano.tensor as T
+
+from treeano.sandbox.nodes.triplet_network import (triplet_network_indices,
+                                                   classification_triplet_loss,
+                                                   l2_norm)
+
+fX = theano.config.floatX
+
+
+def test_triplet_network_indices():
+    for y in [np.random.randint(0, 20, 300).astype(np.int32),
+              np.random.randint(0, 2, 256).astype(np.int32),
+              np.array([0, 1, 0, 1, 1, 1, 0], dtype=np.int32),
+              np.array([0, 0, 1, 1], dtype=np.int32)]:
+        y_true = T.constant(y)
+        symbolic_idxs = triplet_network_indices(y_true)
+        idxs = symbolic_idxs.eval()
+        assert np.alltrue(y[idxs[:, 0]] == y[idxs[:, 1]])
+        assert np.alltrue(y[idxs[:, 2]] != y[idxs[:, 1]])
+
+
+def test_classification_triplet_loss():
+    # NOTE: can be slow if compilation not cached
+    y = np.array([0, 0, 1, 1], dtype=np.int32)
+    y_true = T.constant(y)
+    embeddings = theano.shared(np.random.randn(4, 128).astype(fX))
+    loss = classification_triplet_loss(embeddings, y_true)
+    grad = T.grad(loss, [embeddings])[0]
+    # SGD
+    new_embeddings = (embeddings - 0.01 * grad)
+    # set embeddings to have norm of 1
+    new_embeddings2 = (new_embeddings
+                       / l2_norm(new_embeddings, axis=1, keepdims=True))
+    fn = theano.function([], [loss], updates={embeddings: new_embeddings2})
+    prev_loss = np.inf
+    for _ in range(200):
+        l = fn()[0]
+        assert l < prev_loss
+    import scipy.spatial.distance
+    vecs = embeddings.get_value()
+    dist_matrix = scipy.spatial.distance.cdist(vecs, vecs)
+
+    for row, same, differents in [(0, 1, (2, 3)),
+                                  (1, 0, (2, 3)),
+                                  (2, 3, (0, 1)),
+                                  (3, 2, (0, 1))]:
+        for different in differents:
+            assert dist_matrix[row, same] < dist_matrix[row, different], dict(
+                dist_matrix=dist_matrix,
+                row=row,
+                same=same,
+                different=different,
+            )
+
+
+def test_classification_triplet_same_label():
+    # NOTE: can be slow if compilation not cached
+    # test what happens when there are no triplets (all have the same label)
+    y = np.array([0, 0, 0], dtype=np.int32)
+    y_true = T.constant(y)
+    embeddings = theano.shared(np.random.randn(3, 128).astype(fX))
+    loss = classification_triplet_loss(embeddings, y_true)
+    loss.eval()
+
+
+def test_classification_triplet_same_embedding():
+    # NOTE: can be slow if compilation not cached
+    # test what happens when all triplets have the same label
+    y = np.array([0, 1, 0, 1], dtype=np.int32)
+    y_true = T.constant(y)
+    embeddings = theano.shared(np.zeros((4, 128), dtype=fX))
+    loss = classification_triplet_loss(embeddings, y_true)
+    embeddings_g = T.grad(loss, embeddings)
+    embeddings_g.eval()
---
+++ treeano/sandbox/nodes/triplet_network.py
@@ -0,0 +1,81 @@
+"""
+based on http://arxiv.org/abs/1412.6622, but using a technique that generates
+all possible (same, same, different) triplets in a given minibatch
+
+rationale: using a normal triplet network, the entire network needs to run 3
+times to get 1 triplet for a total of batch_size / 3 triplets. instead, we look
+at every valid triplet in a minibatch, resulting in an expected
+O(batch_size^3) triplets, while also not requiring the user to change how
+minibatches are created (ie. no need to manually create the
+(same, same, different) pattern in each minibatch)
+
+NOTE: there may be a more efficient way to do this by precomputing the distance
+between each pair of embeddings in a minibatch (eg. O(b^2 * k + b^3)) instead
+of computing O(n^3) distances (eg. O(b^3 * k))
+  b = batch size
+  k = embedding size
+"""
+
+from collections import defaultdict
+import itertools
+
+import numpy as np
+import theano
+import theano.tensor as T
+from theano.compile.ops import as_op
+
+
+@as_op(itypes=[theano.tensor.ivector],
+       otypes=[theano.tensor.imatrix])
+def triplet_network_indices(targets):
+    """
+    returns a matrix of the form
+    [[pos_idx_1, ref_idx_1, neg_idx_1],
+     ...
+     [pos_idx_n, ref_idx_n, neg_idx_n]]
+    """
+    # group indices by class
+    grouped = defaultdict(set)
+    for idx, target in enumerate(targets):
+        grouped[target].add(idx)
+    # create triplets
+    all_idxs = set(range(len(targets)))
+    triples = []
+    for pos_idxs in grouped.values():
+        neg_idxs = all_idxs - pos_idxs
+        for pos_idx, ref_idx in itertools.permutations(pos_idxs, 2):
+            for neg_idx in neg_idxs:
+                triples.append([pos_idx, ref_idx, neg_idx])
+    if len(triples) > 0:
+        return np.array(triples, dtype=np.int32)
+    else:
+        return np.zeros((0, 3), dtype=np.int32)
+
+
+def l2_norm(x, axis=None, keepdims=False):
+    # Adding epsilon for numerical stability.
+    # The gradient of sqrt at zero is otherwise undefined.
+    epsilon = 1e-8
+    return T.sqrt(T.sum(x ** 2, axis=axis, keepdims=keepdims) + epsilon)
+
+
+def classification_triplet_loss(embeddings, targets):
+    """
+    embeddings:
+    floatX matrix of shape (batch_size, embedding_size)
+
+    targets:
+    theano int32 vector (ivector)
+    """
+    idxs = triplet_network_indices(targets)
+    positives = embeddings[idxs[:, 0]]
+    references = embeddings[idxs[:, 1]]
+    negatives = embeddings[idxs[:, 2]]
+    pos_l2_norm = l2_norm(positives - references, axis=1)
+    neg_l2_norm = l2_norm(negatives - references, axis=1)
+    max_l2_norm = T.maximum(pos_l2_norm, neg_l2_norm)
+    pos_exp_dist = T.exp(pos_l2_norm - max_l2_norm)
+    neg_exp_dist = T.exp(neg_l2_norm - max_l2_norm)
+    losses = pos_exp_dist / (pos_exp_dist + neg_exp_dist)
+    loss_mean = T.mean(losses)
+    return loss_mean
