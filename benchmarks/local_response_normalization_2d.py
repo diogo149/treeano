7542a8d9295ce0150a1d2e5d51e608113aa0d100
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-10-04 01:27:46 -0700

    add even larger lrn benchmark

--- benchmarks/local_response_normalization_2d.py
+++ benchmarks/local_response_normalization_2d.py
@@ -6,12 +6,13 @@ from treeano.sandbox.nodes import lrn
 fX = theano.config.floatX
 
 # shape = (3, 4, 5, 6)
-shape = (32, 32, 32, 32)
+# shape = (32, 32, 32, 32)
+shape = (128, 32, 128, 128)
 
-# f = lrn.local_response_normalization_2d_v1
+f = lrn.local_response_normalization_2d_v1
 # f = lrn.local_response_normalization_2d_v2
 # f = lrn.local_response_normalization_2d_dnn
-f = lrn.local_response_normalization_2d_pool
+# f = lrn.local_response_normalization_2d_pool
 
 vw = treeano.VariableWrapper("foo",
                              variable=T.tensor4(),
@@ -68,4 +69,18 @@ forward + backward pass:
 local_response_normalization_2d_v1: 6.71 ms
 local_response_normalization_2d_v2: 6.71 ms
 local_response_normalization_2d_pool: 2.69 ms
+
+=====
+
+shape = (128, 32, 128, 128)
+
+forward pass:
+%timeit fn1(x)
+local_response_normalization_2d_v1: 145 ms
+local_response_normalization_2d_pool: 139 ms
+
+forward + backward pass:
+%timeit fn2(x)
+local_response_normalization_2d_v1: 584 ms
+local_response_normalization_2d_pool: 167 ms
 """
