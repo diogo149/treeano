7c40ba727fe63f9bc8bd1fa2d71d411bad213912
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-10-06 23:45:49 -0700

    add soft cross entropy functions

--- treeano/sandbox/utils.py
+++ treeano/sandbox/utils.py
@@ -1,6 +1,8 @@
 import theano
 import theano.tensor as T
 
+fX = theano.config.floatX
+
 
 def cross_covariance(y, z):
     """
@@ -15,3 +17,28 @@ def cross_covariance(y, z):
                   z_centered.dimshuffle(0, 'x', 1))  # (n, i, j)
     C = 0.5 * T.sum(T.sqr(T.mean(outer_prod, axis=0)))
     return C
+
+
+def soft_categorical_crossentropy_i32(pred, target, alpha=0.01):
+    """
+    softer cross-entropy function, where target is treated at not being
+    exactly 1, and instead spreading that probability uniformly to other
+    classes
+    """
+    assert target.dtype == "int32"
+    assert target.ndim == 1
+    assert pred.dtype == fX
+    assert pred.ndim == 2
+    nb_class = pred.shape[1]
+    alpha = 0.01
+    t = T.extra_ops.to_one_hot(target, nb_class=nb_class, dtype=pred.dtype)
+    t = T.clip(t, alpha / (nb_class.astype(fX) - 1.0), 1 - alpha)
+    return T.nnet.categorical_crossentropy(pred, t)
+
+
+def soft_binary_crossentropy(pred, target, alpha=0.01):
+    """
+    softer binary cross-entropy function, where target is treated at not being
+    exactly 1
+    """
+    return T.nnet.binary_crossentropy(pred, T.clip(target, alpha, 1 - alpha))
