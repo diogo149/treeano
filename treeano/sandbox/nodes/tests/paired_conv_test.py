951e825b60d9e8d82345b467888be5ec45f48abb
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-10-13 00:46:34 -0700

    tests for paired conv node

---
+++ treeano/sandbox/nodes/tests/paired_conv_test.py
@@ -0,0 +1,29 @@
+import nose.tools as nt
+import numpy as np
+import theano
+import theano.tensor as T
+
+import treeano
+import treeano.nodes as tn
+from treeano.sandbox.nodes import paired_conv
+
+fX = theano.config.floatX
+
+
+def test_paired_conv_2d_with_bias_node_serialization():
+    tn.check_serialization(paired_conv.PairedConv2DWithBiasNode("a"))
+
+
+def test_paired_conv_2d_with_bias_node():
+    network = tn.SequentialNode(
+        "s",
+        [tn.InputNode("i", shape=(3, 4, 5, 6)),
+         paired_conv.PairedConv2DWithBiasNode(
+             "c",
+             filter_size=(2, 2),
+             num_filters=7,
+             pad="same")]
+    ).network()
+    fn = network.function(["i"], ["s"])
+    res = fn(np.random.randn(3, 4, 5, 6).astype(fX))[0]
+    np.testing.assert_equal((3, 7, 5, 6), res.shape)
