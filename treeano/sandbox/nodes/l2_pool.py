90d93ccdef7dc66f67d649874a024df453eeb5c1
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-10-14 23:11:56 -0700

    add l2 pool node

---
+++ treeano/sandbox/nodes/l2_pool.py
@@ -0,0 +1,51 @@
+import numpy as np
+import theano
+import theano.tensor as T
+
+import treeano
+import treeano.nodes as tn
+
+fX = theano.config.floatX
+
+
+@treeano.register_node("l2_pool")
+class L2PoolNode(treeano.Wrapper1NodeImpl):
+
+    """
+    node that takes the L2 norm of the pooled over region
+    """
+
+    hyperparameter_names = ("pool_size",)
+
+    def architecture_children(self):
+        nodes = [
+            tn.SqrNode(self.name + "_sqr"),
+            self._children.children,
+            # convert mean pool to sum pool by multiplying by pool size
+            tn.MultiplyConstantNode(self.name + "_mul"),
+            tn.SqrtNode(self.name + "_sqrt"),
+        ]
+        return [tn.SequentialNode(self.name + "_sequential", nodes)]
+
+    def init_state(self, network):
+        super(L2PoolNode, self).init_state(network)
+        pool_size = network.find_hyperparameter(["pool_size"])
+        network.set_hyperparameter(self.name + "_mul",
+                                   "value",
+                                   # cast to float, to not trigger
+                                   # warn_float64
+                                   float(np.prod(pool_size)))
+
+
+def L2Pool2DNode(name, pool_size, **kwargs):
+    return L2PoolNode(
+        name,
+        tn.MeanPool2DNode(name + "_pool", **kwargs),
+        pool_size=pool_size)
+
+
+def DnnL2Pool2DNode(name, pool_size, **kwargs):
+    return L2PoolNode(
+        name,
+        tn.DnnMeanPoolNode(name + "_pool", **kwargs),
+        pool_size=pool_size)
