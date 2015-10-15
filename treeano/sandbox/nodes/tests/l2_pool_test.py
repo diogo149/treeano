88e14b900b2121a6eaa9e7e7bd4b53750b9e6276
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-10-14 23:12:04 -0700

    add tests for l2 pool node

---
+++ treeano/sandbox/nodes/tests/l2_pool_test.py
@@ -0,0 +1,30 @@
+import nose.tools as nt
+import numpy as np
+import theano
+import theano.tensor as T
+
+import treeano
+import treeano.nodes as tn
+from treeano.sandbox.nodes import l2_pool
+
+fX = theano.config.floatX
+
+
+def test_l2_pool_2d_node_serialization():
+    # NOTE: serialization converts pool_size to list, so must be a list
+    tn.check_serialization(l2_pool.L2Pool2DNode("a", pool_size=[1, 1]))
+
+
+def test_l2_pool_2d_node():
+    network = tn.SequentialNode(
+        "s",
+        [tn.InputNode("i", shape=(1, 1, 4, 4)),
+         l2_pool.L2Pool2DNode("p", pool_size=(2, 2))]).network()
+    fn = network.function(["i"], ["s"])
+    x = np.array([[[[3, 4, 1, 2],
+                    [0, 0, 3, 4],
+                    [1, 1, -1, 1],
+                    [1, 1, 1, -1]]]], dtype=fX)
+    ans = np.array([[[[5, np.linalg.norm([1, 2, 3, 4])],
+                      [2, 2]]]], dtype=fX)
+    np.testing.assert_allclose(ans, fn(x)[0])
