6ba2dc8cf06efd74cae941c370e75ccddcf1d25c
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-10-15 00:24:52 -0700

    fix broken arg of DnnL2Pool2DNode

--- treeano/sandbox/nodes/l2_pool.py
+++ treeano/sandbox/nodes/l2_pool.py
@@ -47,7 +47,7 @@ def L2Pool2DNode(name, **kwargs):
         **l2_kwargs)
 
 
-def DnnL2Pool2DNode(name, pool_size, **kwargs):
+def DnnL2Pool2DNode(name, **kwargs):
     l2_kwargs = {}
     if "pool_size" in kwargs:
         l2_kwargs["pool_size"] = kwargs.pop("pool_size")
