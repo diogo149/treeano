655e2c6ebaa0b3d3a404d14293c6e17f11eed416
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-10-23 16:41:31 -0700

    pep8 mixed pooling

--- treeano/sandbox/nodes/mixed_pooling.py
+++ treeano/sandbox/nodes/mixed_pooling.py
@@ -11,52 +11,51 @@ fX = theano.config.floatX
 @treeano.register_node("mixed_pool")
 class MixedPoolNode(treeano.Wrapper0NodeImpl):
 
-        hyperparameter_names = (
-            tuple([x
-                   for x in tn.DnnPoolNode.hyperparameter_names
-                   if x != "mode"])
-            + ("learnable",))
+    hyperparameter_names = (
+        tuple([x
+               for x in tn.DnnPoolNode.hyperparameter_names
+               if x != "mode"])
+        + ("learnable",))
 
-        def architecture_children(self):
-            mean_seq_node = tn.SequentialNode(
-                self.name + "_mean_seq",
-                [tn.DnnMeanPoolNode(self.name + "_mean_pool"),
-                 tn.MultiplyConstantNode(self.name +"_mean_const_mult")]
-            )
+    def architecture_children(self):
+        mean_seq_node = tn.SequentialNode(
+            self.name + "_mean_seq",
+            [tn.DnnMeanPoolNode(self.name + "_mean_pool"),
+             tn.MultiplyConstantNode(self.name + "_mean_const_mult")]
+        )
 
-            max_seq_node = tn.SequentialNode(
-                self.name + "_max_seq",
-                [tn.DnnMaxPoolNode(self.name + "_max_pool"),
-                 tn.MultiplyConstantNode(self.name +"_max_const_mult")]
-            )
+        max_seq_node = tn.SequentialNode(
+            self.name + "_max_seq",
+            [tn.DnnMaxPoolNode(self.name + "_max_pool"),
+             tn.MultiplyConstantNode(self.name + "_max_const_mult")]
+        )
 
-            return [tn.ElementwiseSumNode(self.name +"_sum_mixed",
-                                          [max_seq_node, mean_seq_node])]
-                    
-        def init_state(self, network):
-            super(MixedPoolNode, self).init_state(network)
-            learnable = network.find_hyperparameter(["learnable"], False)
+        return [tn.ElementwiseSumNode(self.name + "_sum_mixed",
+                                      [max_seq_node, mean_seq_node])]
 
-            # TODO parameterize init alpha
-            if learnable:
-                inits = list(toolz.concat(network.find_hyperparameters(
-                    ["inits"],
-                    [treeano.inits.ConstantInit(0.5)])))
-                
-                alpha = network.create_vw(
-                    "alpha",
-                    shape=(),
-                    is_shared=True,
-                    tags = {"parameter"},
-                    inits=inits).variable
+    def init_state(self, network):
+        super(MixedPoolNode, self).init_state(network)
+        learnable = network.find_hyperparameter(["learnable"], False)
 
-            else:
-                alpha = 0.5
+        # TODO parameterize init alpha
+        alpha = 0.5
+        if learnable:
+            inits = list(toolz.concat(network.find_hyperparameters(
+                ["inits"],
+                [treeano.inits.ConstantInit(alpha)])))
 
-            network.set_hyperparameter(self.name + "_max_const_mult",
-                                       "value",
-                                       alpha)
-                
-            network.set_hyperparameter(self.name + "_mean_const_mult",
-                                       "value",
-                                       1 - alpha)
+            alpha = network.create_vw(
+                "alpha",
+                shape=(),
+                is_shared=True,
+                tags = {"parameter"},
+                inits=inits
+            ).variable
+
+        network.set_hyperparameter(self.name + "_max_const_mult",
+                                   "value",
+                                   alpha)
+
+        network.set_hyperparameter(self.name + "_mean_const_mult",
+                                   "value",
+                                   1 - alpha)
