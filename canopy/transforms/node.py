0fd2376a6d4572fe40f44919e4ce2d1852841e31
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-10-25 13:29:38 -0700

    add remove_nodes_with_class transform

--- canopy/transforms/node.py
+++ canopy/transforms/node.py
@@ -8,18 +8,27 @@ import treeano.nodes as tn
 from . import fns
 
 
-def remove_dropout(network, **kwargs):
+def remove_nodes_with_class(network, cls, **kwargs):
     """
-    replaced DropoutNode's with IdentityNode's with the same name
+    replaced nodes of a given class with IdentityNode's with the same name
     """
 
     def inner(node):
-        if isinstance(node, tn.DropoutNode):
+        if isinstance(node, cls):
             return tn.IdentityNode(node.name)
         else:
             return node
 
     return fns.transform_root_node_postwalk(network, inner, **kwargs)
+
+
+def remove_dropout(network, **kwargs):
+    """
+    replaced DropoutNode's with IdentityNode's with the same name
+
+    NOTE: only removes bernoulli dropout nodes
+    """
+    return remove_nodes_with_class(network, tn.DropoutNode, **kwargs)
 
 
 def replace_node(network, name_to_node, **kwargs):
