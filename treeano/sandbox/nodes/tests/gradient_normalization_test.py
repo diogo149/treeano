fix gradient normalization test for new defaults

treeano/sandbox/nodes/tests/gradient_normalization_test.py
@@ -12,7 +12,10 @@ fX = theano.config.floatX
 
 
 def test_gradient_batch_normalization_op():
-    op = gn.GradientBatchNormalizationOp()
+    epsilon = 1e-8
+    op = gn.GradientBatchNormalizationOp(subtract_mean=True,
+                                         keep_mean=False,
+                                         epsilon=epsilon)
 
     X = np.random.randn(3, 4).astype(fX)
     W = np.random.randn(2, 3).astype(fX)
@@ -23,7 +26,7 @@ def test_gradient_batch_normalization_op():
     orig_grad = T.grad(w.dot(x).sum(), x).eval({x: X, w: W})
     new_grad = T.grad(w.dot(op(x)).sum(), x).eval({x: X, w: W})
     mu = orig_grad.mean(axis=0, keepdims=True)
-    sigma = orig_grad.std(axis=0, keepdims=True) + 1e-8
+    sigma = orig_grad.std(axis=0, keepdims=True) + epsilon
     ans = (orig_grad - mu) / sigma
     np.testing.assert_allclose(ans,
                                new_grad,