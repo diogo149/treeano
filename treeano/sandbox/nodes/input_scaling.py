f0c2938dfcf3778116a7774a66072f603046ceb7
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-10-29 01:10:33 -0700

    add tanh scaling node

--- treeano/sandbox/nodes/input_scaling.py
+++ treeano/sandbox/nodes/input_scaling.py
@@ -57,10 +57,11 @@ class ClipScalingNode(treeano.NodeImpl):
         param_pattern.insert(2, 0)
 
         in_b = in_vw.variable.dimshuffle(*in_pattern)
-
         mins_b = mins_var.dimshuffle(*param_pattern)
         maxs_b = maxs_var.dimshuffle(*param_pattern)
+
         range_b = maxs_b - mins_b
+        # TODO constrain range to be > 0?
         clipped = T.clip(in_b - mins_b, 0, range_b)
         scaled = clipped / range_b
 
@@ -79,4 +80,78 @@ class ClipScalingNode(treeano.NodeImpl):
             variable=out_var,
             shape=out_shape,
             tags={}
+        )
+
+
+@treeano.register_node("tanh_scaling")
+class TanhScalingNode(treeano.NodeImpl):
+
+    hyperparameter_names = ("learnable",
+                            "means",
+                            "scales")
+
+    def compute_output(self, network, in_vw):
+        learnable = network.find_hyperparameter(["learnable"], False)
+        means = network.find_hyperparameter(["means"])
+        scales = network.find_hyperparameter(["scales"])
+        assert means.ndim == scales.ndim == 1
+        assert means.shape == scales.shape
+        means = treeano.utils.as_fX(means)
+        scales = treeano.utils.as_fX(scales)
+        num_scales = means.shape[0]
+
+        if learnable:
+            means_inits = list(toolz.concat(network.find_hyperparameters(
+                ["inits"],
+                [treeano.inits.ConstantInit(means)])))
+            scales_inits = list(toolz.concat(network.find_hyperparameters(
+                ["inits"],
+                [treeano.inits.ConstantInit(scales)])))
+            means_var = network.create_vw(
+                "means",
+                shape=means.shape,
+                is_shared=True,
+                tags={"parameter"},
+                inits=means_inits,
+            ).variable
+            scales_var = network.create_vw(
+                "scales",
+                shape=scales.shape,
+                is_shared=True,
+                tags={"parameter"},
+                inits=scales_inits,
+            ).variable
+        else:
+            means_var = T.constant(means)
+            scales_var = T.constant(scales)
+
+        in_pattern = list(range(in_vw.ndim))
+        # insert after channel dim
+        in_pattern.insert(2, "x")
+
+        param_pattern = ["x"] * in_vw.ndim
+        param_pattern.insert(2, 0)
+
+        in_b = in_vw.variable.dimshuffle(*in_pattern)
+        means_b = means_var.dimshuffle(*param_pattern)
+        scales_b = scales_var.dimshuffle(*param_pattern)
+
+        # TODO constrain scales to be > 0?
+        scaled = T.tanh((in_b - means_b) / scales_b)
+
+        # reshape newly created dim into dim 1
+        out_ss = list(in_vw.symbolic_shape())
+        out_ss[1] *= num_scales
+        out_var = scaled.reshape(tuple(out_ss))
+
+        out_shape = list(in_vw.shape)
+        if out_shape[1] is not None:
+            out_shape[1] *= num_scales
+        out_shape = tuple(out_shape)
+
+        network.create_vw(
+            "default",
+            variable=out_var,
+            shape=out_shape,
+            tags={"output"},
         )
