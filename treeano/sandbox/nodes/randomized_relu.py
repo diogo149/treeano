65001b4b8dcba79aafe23eeccdea0fb03f2353dd
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-11-03 01:08:17 -0800

    add uniform randomized relu

--- treeano/sandbox/nodes/randomized_relu.py
+++ treeano/sandbox/nodes/randomized_relu.py
@@ -2,7 +2,6 @@
 from "Empirical Evaluation of Rectified Activations in Convolutional Network"
 http://arxiv.org/abs/1505.00853
 """
-import toolz
 import numpy as np
 import theano
 import theano.tensor as T
@@ -35,6 +34,46 @@ class RandomizedReLUNode(treeano.NodeImpl):
                                   low=l,
                                   high=u)
             negative_coefficient = 1.0 / alphas
+
+        # return output
+        network.create_vw(
+            "default",
+            variable=treeano.utils.rectify(
+                in_vw.variable,
+                negative_coefficient=negative_coefficient),
+            shape=in_vw.shape,
+            tags={"output"},
+        )
+
+
+@treeano.register_node("uniform_randomized_relu")
+class UniformRandomizedReLUNode(treeano.NodeImpl):
+
+    """
+    like RandomizedReLUNode, but instead of sampling from 1 / uniform(l, u),
+    sample from uniform(l, u)
+    """
+
+    hyperparameter_names = ("alpha_lower",
+                            "alpha_upper",
+                            "deterministic")
+
+    def compute_output(self, network, in_vw):
+        # gather hyperparameters
+        deterministic = network.find_hyperparameter(["deterministic"])
+        l = network.find_hyperparameter(["alpha_lower"],
+                                        1 / 8.)
+        u = network.find_hyperparameter(["alpha_upper"],
+                                        1 / 3.)
+
+        if deterministic:
+            negative_coefficient = (l + u) / 2.
+        else:
+            # TODO save this state so that we can seed the rng
+            srng = MRG_RandomStreams()
+            negative_coefficient = srng.uniform(size=in_vw.symbolic_shape(),
+                                                low=l,
+                                                high=u)
 
         # return output
         network.create_vw(
