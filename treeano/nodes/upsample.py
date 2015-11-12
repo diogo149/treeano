c651f3d8ddf7a8664243f260a2112223fe705fcd
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-11-11 18:41:00 -0800

    add normalize flag for repeat upsample

--- treeano/nodes/upsample.py
+++ treeano/nodes/upsample.py
@@ -1,3 +1,4 @@
+import numpy as np
 import theano
 import theano.tensor as T
 
@@ -12,13 +13,17 @@ class RepeatNDNode(core.NodeImpl):
     repeats each axis of a tensor by a specified amount
 
     also referred to as "nearest neighbor" upsampling
+
+    the normalize hyperparameter divides the output by the upsample factor,
+    so that the total sum of the input and output are the same
     """
 
-    hyperparameter_names = ("upsample_factor",)
+    hyperparameter_names = ("upsample_factor", "normalize")
 
     def compute_output(self, network, in_vw):
         upsample_factor = network.find_hyperparameter(["upsample_factor"])
         assert len(upsample_factor) == in_vw.ndim
+        normalize = network.find_hyperparameter(["normalize"], False)
 
         out_var = in_vw.variable
         out_shape = list(in_vw.shape)
@@ -32,6 +37,9 @@ class RepeatNDNode(core.NodeImpl):
                 if out_shape[axis] is not None:
                     out_shape[axis] *= factor
 
+        if normalize:
+            out_var = out_var / utils.as_fX(np.prod(upsample_factor))
+
         network.create_vw(
             "default",
             variable=out_var,
@@ -40,11 +48,13 @@ class RepeatNDNode(core.NodeImpl):
         )
 
 
-def SpatialRepeatNDNode(name, upsample_factor):
+def SpatialRepeatNDNode(name, upsample_factor, **kwargs):
     """
     same as RepeatNDNode, but assumes first 2 axes will not be upsampled
     """
-    return RepeatNDNode(name, upsample_factor=(1, 1) + upsample_factor)
+    return RepeatNDNode(name,
+                        upsample_factor=(1, 1) + upsample_factor,
+                        **kwargs)
 
 
 @core.register_node("sparse_upsample")
