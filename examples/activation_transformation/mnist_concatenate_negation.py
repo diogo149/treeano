358a6e1aa72bea820d79024e9cd8a80a5f30afc9
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-12-03 09:59:06 -0800

    add concatenate negation example

---
+++ examples/activation_transformation/mnist_concatenate_negation.py
@@ -0,0 +1,105 @@
+from __future__ import division, absolute_import
+from __future__ import print_function, unicode_literals
+
+import itertools
+import numpy as np
+import sklearn.datasets
+import sklearn.cross_validation
+import sklearn.metrics
+import theano
+import theano.tensor as T
+import treeano
+import treeano.nodes as tn
+from treeano.sandbox.nodes import activation_transformation as at
+import canopy
+import canopy.sandbox.datasets
+
+fX = theano.config.floatX
+
+# ############################### prepare data ###############################
+
+train, valid, test = canopy.sandbox.datasets.mnist()
+
+# ############################## prepare model ##############################
+
+model = tn.HyperparameterNode(
+    "model",
+    tn.SequentialNode(
+        "seq",
+        [tn.InputNode("x", shape=(None, 1, 28, 28)),
+         tn.Conv2DWithBiasNode("conv1"),
+         at.ConcatenateNegationNode("cn1"),
+         tn.ReLUNode("relu1"),
+         tn.MaxPool2DNode("mp1"),
+         tn.Conv2DWithBiasNode("conv2"),
+         at.ConcatenateNegationNode("cn2"),
+         tn.ReLUNode("relu2"),
+         tn.MaxPool2DNode("mp2"),
+         tn.DenseNode("fc1"),
+         at.ConcatenateNegationNode("cn3"),
+         tn.ReLUNode("relu3"),
+         tn.DropoutNode("do1"),
+         tn.DenseNode("fc2", num_units=10),
+         tn.SoftmaxNode("pred"),
+         ]),
+    num_filters=32,
+    filter_size=(5, 5),
+    pool_size=(2, 2),
+    num_units=256,
+    dropout_probability=0.5,
+    inits=[at.NegatedInit(treeano.inits.XavierNormalInit())],
+)
+
+with_updates = tn.HyperparameterNode(
+    "with_updates",
+    tn.AdamNode(
+        "adam",
+        {"subtree": model,
+         "cost": tn.TotalCostNode("cost", {
+             "pred": tn.ReferenceNode("pred_ref", reference="model"),
+             "target": tn.InputNode("y", shape=(None,), dtype="int32")},
+         )}),
+    cost_function=treeano.utils.categorical_crossentropy_i32,
+)
+network = with_updates.network()
+network.build()  # build eagerly to share weights
+
+BATCH_SIZE = 500
+
+valid_fn = canopy.handled_fn(
+    network,
+    [canopy.handlers.time_call(key="valid_time"),
+     canopy.handlers.override_hyperparameters(dropout_probability=0),
+     canopy.handlers.chunk_variables(batch_size=BATCH_SIZE,
+                                     variables=["x", "y"])],
+    {"x": "x", "y": "y"},
+    {"cost": "cost", "pred": "pred"})
+
+
+def validate(in_dict, results_dict):
+    valid_out = valid_fn(valid)
+    probabilities = valid_out["pred"]
+    predicted_classes = np.argmax(probabilities, axis=1)
+    results_dict["valid_cost"] = valid_out["cost"]
+    results_dict["valid_time"] = valid_out["valid_time"]
+    results_dict["valid_accuracy"] = sklearn.metrics.accuracy_score(
+        valid["y"], predicted_classes)
+
+train_fn = canopy.handled_fn(
+    network,
+    [canopy.handlers.time_call(key="total_time"),
+     canopy.handlers.call_after_every(1, validate),
+     canopy.handlers.time_call(key="train_time"),
+     canopy.handlers.chunk_variables(batch_size=BATCH_SIZE,
+                                     variables=["x", "y"])],
+    {"x": "x", "y": "y"},
+    {"train_cost": "cost"},
+    include_updates=True)
+
+
+# ################################# training #################################
+
+print("Starting training...")
+canopy.evaluate_until(fn=train_fn,
+                      gen=itertools.repeat(train),
+                      max_iters=25)
