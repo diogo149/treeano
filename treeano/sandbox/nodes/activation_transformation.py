de7745c6d41ae6cc4435fa9543278c1a71154166
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-12-03 09:46:31 -0800

    add init for concatenate negation

--- treeano/sandbox/nodes/activation_transformation.py
+++ treeano/sandbox/nodes/activation_transformation.py
@@ -34,3 +34,40 @@ class ConcatenateNegationNode(treeano.NodeImpl):
             shape=out_shape,
             tags={"output"},
         )
+
+
+class NegatedInit(treeano.inits.WeightInit):
+
+    """
+    init specifically for units after ConcatenateNegationNode
+
+    takes in a different init, and initializes the first half of with that
+    init, and the second half with the negated version of that tensor
+
+    rationale: ConcatenateNegationNode + this init + ReLU will initialize
+    the network to be linear
+    """
+
+    def __init__(self, init, axis=1):
+        self.init = init
+        self.axis = axis
+
+    def initialize_value(self, vw):
+        # temporary variable wrapper with fake shape
+        tmp_vw_shape = list(vw.shape)
+        if tmp_vw_shape[self.axis] % 2 != 0:
+            # this weight is probably not after a ConcatenateNegationNode,
+            # so instead revert to initial init
+            return self.init.initialize_value(vw)
+        tmp_vw_shape[self.axis] /= 2
+        tmp_vw_shape = tuple(tmp_vw_shape)
+        tmp_vw = treeano.VariableWrapper(
+            "tmp",
+            shape=tmp_vw_shape,
+            is_shared=True,
+            inits=[],
+        )
+
+        val = self.init.initialize_value(tmp_vw)
+
+        return np.concatenate([val, -val], axis=self.axis)
