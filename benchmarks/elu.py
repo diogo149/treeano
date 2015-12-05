38882152291a89d073d93dacb4485300f1cff6ed
Diogo Moitinho de Almeida <diogo149@gmail.com>
2015-12-04 17:55:53 -0800

    add ELU benchmark

---
+++ benchmarks/elu.py
@@ -0,0 +1,37 @@
+import numpy as np
+import theano
+import theano.tensor as T
+import treeano.nodes as tn
+fX = theano.config.floatX
+
+
+def elu1(x, alpha=1.):
+    return T.switch(T.gt(x, 0.), x, alpha * (T.exp(x) - 1))
+
+
+def elu2(x, alpha=1.):
+    pos = (x + abs(x)) / 2
+    neg = (-x + abs(-x)) / 2
+    return pos + alpha * (T.exp(neg) - 1)
+
+
+# TODO change me
+elu = elu1
+# elu = elu2
+
+x = T.matrix()
+f = elu(x)
+b = T.grad(f.sum(), x)
+X = np.random.randn(4096, 4096).astype(fX)
+
+"""
+20151204 results
+
+%timeit f.eval({x: X})
+elu1 => 33.3 ms
+elu2 => 31.8 ms
+
+%timeit b.eval({x: X})
+elu1 => 161 ms
+elu2 => 32.2 ms
+"""
