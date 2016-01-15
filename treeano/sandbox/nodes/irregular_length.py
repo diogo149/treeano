1108659cb1e0bf293dbaaf7178606edf05e21212
Diogo Moitinho de Almeida <diogo149@gmail.com>
2016-01-15 01:13:19 -0800

    add irregular length mean

--- treeano/sandbox/nodes/irregular_length.py
+++ treeano/sandbox/nodes/irregular_length.py
@@ -25,3 +25,29 @@ class UngroupIrregularLengthTensorsNode(treeano.NodeImpl):
             shape=(None, None) + in_vw.shape[1:],
             tags={"output"},
         )
+
+
+@treeano.register_node("irregular_length_mean")
+class IrregularLengthMeanNode(treeano.NodeImpl):
+
+    """
+    meant to take in input from UngroupIrregularLengthTensorsNode
+    """
+
+    hyperparameter_names = ("lengths_reference",)
+    input_keys = ("default", "lengths")
+
+    def init_long_range_dependencies(self, network):
+        network.take_output_from(
+            network.find_hyperparameter(["lengths_reference"]),
+            to_key="lengths")
+
+    def compute_output(self, network, in_vw, lengths_vw):
+        lengths = lengths_vw.variable.dimshuffle([0] + ["x"] * (in_vw.ndim - 2))
+        network.create_vw(
+            "default",
+            variable=in_vw.variable.sum(axis=1) / lengths,
+            # drop axis 1
+            shape=in_vw.shape[0:1] + in_vw.shape[2:],
+            tags={"output"},
+        )
