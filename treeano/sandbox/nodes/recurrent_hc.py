d62e01e3e55aa8d5d4843ce513362548be4f4087
Diogo Moitinho de Almeida <diogo149@gmail.com>
2016-04-09 11:58:09 -0700

    add LSTM implementation

--- treeano/sandbox/nodes/recurrent_hc.py
+++ treeano/sandbox/nodes/recurrent_hc.py
@@ -131,3 +131,133 @@ class RNNNode(treeano.NodeImpl):
                 shape=out_shape,
                 tags={"output"},
             )
+
+
+@treeano.register_node("hc_lstm")
+class LSTMNode(treeano.NodeImpl):
+
+    hyperparameter_names = ("inits",
+                            "num_units",
+                            "learn_init",
+                            "backwards",
+                            "grad_clip",
+                            "only_return_final",
+                            "forget_gate_bias")
+
+    def compute_output(self, network, in_vw):
+        num_units = network.find_hyperparameter(["num_units"])
+        learn_init = network.find_hyperparameter(["learn_init"], False)
+        backwards = network.find_hyperparameter(["backwards"], False)
+        grad_clip = network.find_hyperparameter(["grad_clip"], None)
+        forget_gate_bias = network.find_hyperparameter(["forget_gate_bias"], 0)
+        only_return_final = network.find_hyperparameter(["only_return_final"],
+                                                        False)
+
+        # input should have axes (batch, time, features)
+        assert in_vw.ndim == 3
+        num_inputs = in_vw.shape[-1]
+        assert num_inputs is not None
+
+        i_to_h_weight = network.create_vw(
+            name="i_to_h_weight",
+            is_shared=True,
+            shape=(num_inputs, 4 * num_units),
+            tags={"parameter", "weight"},
+            default_inits=[],
+        ).variable
+        h_to_h_weight = network.create_vw(
+            name="h_to_h_weight",
+            is_shared=True,
+            shape=(num_units, 4 * num_units),
+            tags={"parameter", "weight"},
+            default_inits=[],
+        ).variable
+        b = network.create_vw(
+            name="bias",
+            is_shared=True,
+            shape=(4 * num_units,),
+            tags={"parameter", "bias"},
+            default_inits=[],
+        ).variable
+
+        def make_initial_state(name):
+            if learn_init:
+                initial_state = network.create_vw(
+                    name=name,
+                    is_shared=True,
+                    shape=(num_units,),
+                    tags={"parameter", "bias"},
+                    default_inits=[],
+                ).variable
+            else:
+                initial_state = T.zeros((num_units,), dtype=fX)
+            # repeat initial state for whole minibatch
+            initial_state = T.repeat(initial_state.reshape((1, -1)),
+                                     repeats=in_vw.symbolic_shape()[0],
+                                     axis=0)
+            return initial_state
+
+        initial_cell_state = make_initial_state("initial_cell_state")
+        initial_hidden_state = make_initial_state("initial_hidden_state")
+
+        in_var = in_vw.variable
+        # precompute i to h and bias
+        in_feats = T.dot(in_var, i_to_h_weight) + b.dimshuffle("x", "x", 0)
+        # convert axes to order (time, batch, features)
+        in_feats = in_feats.dimshuffle(1, 0, 2)
+
+        def step(in_feat, c_prev, h_prev, _h_to_h_weight):
+            logit = in_feat + T.dot(h_prev, h_to_h_weight)
+            if grad_clip is not None:
+                logit = theano.gradient.grad_clip(logit, -grad_clip, grad_clip)
+
+            update_logit = logit[:, :num_units]
+            forget_logit = logit[:, num_units:num_units * 2]
+            input_logit = logit[:, num_units * 2:num_units * 3]
+            output_logit = logit[:, num_units * 3:]
+
+            update = T.tanh(update_logit)
+            forget_gate = T.nnet.sigmoid(forget_logit + forget_gate_bias)
+            input_gate = T.nnet.sigmoid(input_logit)
+            output_gate = T.nnet.sigmoid(output_logit)
+
+            c = input_gate * update + forget_gate * c_prev
+            h = output_gate * T.tanh(c)
+            return c, h
+
+        res, _updates = theano.scan(fn=step,
+                                    sequences=[in_feats],
+                                    outputs_info=[initial_cell_state,
+                                                  initial_hidden_state],
+                                    non_sequences=[h_to_h_weight],
+                                    go_backwards=backwards,
+                                    strict=True)
+
+        cells, hidden_states = res
+        out_var = hidden_states
+
+        if only_return_final:
+            out_var = out_var[-1]
+
+            out_shape = (in_vw.shape[0], num_units)
+
+            network.create_vw(
+                name="default",
+                variable=out_var,
+                shape=out_shape,
+                tags={"output"},
+            )
+        else:
+            # convert axes back to (batch, time, features)
+            out_var = out_var.dimshuffle(1, 0, 2)
+            if backwards:
+                out_var = out_var[:, ::-1]
+
+            out_shape = in_vw.shape[:-1] + (num_units,)
+
+            network.create_vw(
+                name="default",
+                variable=out_var,
+                shape=out_shape,
+                tags={"output"},
+            )
