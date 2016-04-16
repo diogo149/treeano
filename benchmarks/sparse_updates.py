7d1d1f31687c641b3efd6ba960805afaa93415a0
Diogo Moitinho de Almeida <diogo149@gmail.com>
2016-04-16 04:51:30 -0700

    more sparse update benchmarks

--- benchmarks/sparse_updates.py
+++ benchmarks/sparse_updates.py
@@ -5,9 +5,32 @@ fX = theano.config.floatX
 s = theano.shared(np.zeros((30000, 10000), dtype=fX))
 fn1 = theano.function([], updates=[(s, s + 1)])
 fn2 = theano.function([], updates=[(s, T.inc_subtensor(s[1497], s[1497] ** 2))])
+# as in update deltas
+fn3 = theano.function([],
+                      updates=[(s, s + (T.inc_subtensor(s[1497], s[1497] ** 2) - s))])
 
 """
 on cpu:
 %timeit fn1()  # 166ms
 %timeit fn2()  # 14.4us
+%timeit fn3()  # 12us
 """
+
+# doesn't work
+cost = (s[0] + s[0] + 3 * s[1]).sum()
+g1, g2 = theano.grad(cost, [s[0], s[1]])
+# DisconnectedInputError: grad method was asked to compute the gradient
+# with respect to a variable that is not part of the computational graph
+# of the cost, or is used only by a non-differentiable operator:
+# Subtensor{int64}.0
+g1.eval()
+g2.eval()
+
+
+# works
+s0 = s[0]
+s1 = s[1]
+cost = (s0 + s0 + 3 * s1).sum()
+g1, g2 = theano.grad(cost, [s0, s1])
+g1.eval()
+g2.eval()
