2354791d83ac0f25c0af4332dc8959627ca2a786
Diogo Moitinho de Almeida <diogo149@gmail.com>
2016-05-14 17:55:08 -0700

    add gradnet examples

---
+++ examples/expected_batches/mnist_mlp_grelu.py
@@ -0,0 +1,99 @@
+from __future__ import division, absolute_import
+from __future__ import print_function, unicode_literals
+
+import itertools
+import numpy as np
+import sklearn.datasets
+import sklearn.cross_validation
+import sklearn.metrics
+import theano
+import theano.tensor as T
+import treeano
+import treeano.nodes as tn
+import canopy
+import canopy.sandbox.datasets
+
+from treeano.sandbox.nodes import expected_batches as eb
+
+fX = theano.config.floatX
+
+# ############################### prepare data ###############################
+
+train, valid, test = canopy.sandbox.datasets.mnist()
+
+# ############################## prepare model ##############################
+
+BATCH_SIZE = 500
+NUM_EPOCHS = 25
+
+model = tn.HyperparameterNode(
+    "model",
+    tn.SequentialNode(
+        "seq",
+        [tn.InputNode("x", shape=(None, 1,  28, 28)),
+         tn.DenseNode("fc1"),
+         eb.GradualReLUNode("relu1"),
+         tn.DropoutNode("do1"),
+         tn.DenseNode("fc2"),
+         eb.GradualReLUNode("relu2"),
+         tn.DropoutNode("do2"),
+         tn.DenseNode("fc3", num_units=10),
+         tn.SoftmaxNode("pred"),
+         ]),
+    num_units=512,
+    dropout_probability=0.5,
+    inits=[treeano.inits.XavierNormalInit()],
+    expected_batches=NUM_EPOCHS * len(train["x"]) / BATCH_SIZE,
+)
+
+with_updates = tn.HyperparameterNode(
+    "with_updates",
+    tn.AdamNode(
+        "adam",
+        {"subtree": model,
+         "cost": tn.TotalCostNode("cost", {
+             "pred": tn.ReferenceNode("pred_ref", reference="model"),
+             "target": tn.InputNode("y", shape=(None,), dtype="int32")},
+         )}),
+    cost_function=treeano.utils.categorical_crossentropy_i32,
+)
+network = with_updates.network()
+network.build()  # build eagerly to share weights
+
+valid_fn = canopy.handled_fn(
+    network,
+    [canopy.handlers.time_call(key="valid_time"),
+     canopy.handlers.override_hyperparameters(dropout_probability=0),
+     canopy.handlers.chunk_variables(batch_size=BATCH_SIZE,
+                                     variables=["x", "y"])],
+    {"x": "x", "y": "y"},
+    {"cost": "cost", "pred": "pred"})
+
+
+def validate(in_dict, results_dict):
+    valid_out = valid_fn(valid)
+    probabilities = valid_out["pred"]
+    predicted_classes = np.argmax(probabilities, axis=1)
+    results_dict["valid_cost"] = valid_out["cost"]
+    results_dict["valid_time"] = valid_out["valid_time"]
+    results_dict["valid_accuracy"] = sklearn.metrics.accuracy_score(
+        valid["y"], predicted_classes)
+
+train_fn = canopy.handled_fn(
+    network,
+    [canopy.handlers.time_call(key="total_time"),
+     canopy.handlers.call_after_every(1, validate),
+     canopy.handlers.time_call(key="train_time"),
+     canopy.handlers.chunk_variables(batch_size=BATCH_SIZE,
+                                     variables=["x", "y"])],
+    {"x": "x", "y": "y"},
+    {"train_cost": "cost"},
+    include_updates=True)
+
+
+# ################################# training #################################
+
+print("Starting training...")
+canopy.evaluate_until(fn=train_fn,
+                      gen=itertools.repeat(train),
+                      max_iters=NUM_EPOCHS)
