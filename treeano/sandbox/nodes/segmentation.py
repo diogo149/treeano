38bf5805a63d007ff765b88453e681b3fe360ec3
Diogo Moitinho de Almeida <diogo149@gmail.com>
2016-05-16 16:15:42 -0700

    add hard boostrapping cross-entropy loss

--- treeano/sandbox/nodes/segmentation.py
+++ treeano/sandbox/nodes/segmentation.py
@@ -1,3 +1,4 @@
+import functools
 import theano
 import theano.tensor as T
 
@@ -11,3 +12,25 @@ def approximate_negative_iou(pred, target, epsilon=1e-3):
     union = T.maximum(pred, target).sum(axis=(1, 2, 3))
     union = T.maximum(union, epsilon)
     return -1.0 * (intersection / union).mean()
+
+
+def hard_bootstrapping_binary_crossentropy(pred,
+                                           target,
+                                           num_taken,
+                                           num_skipped=0):
+    """
+    from
+    High-performance Semantic Segmentation Using Very Deep Fully Convolutional Networks
+    http://arxiv.org/abs/1604.04339
+    """
+    pixel_loss = T.nnet.binary_crossentropy(pred, target)
+    flat_loss = pixel_loss.flatten(2)
+    sorted_flat_loss = T.sort(flat_loss)
+    chosen_loss = sorted_flat_loss[:, -(num_taken + num_skipped):-num_skipped]
+    return chosen_loss
+
+
+def hard_bootstrapping_binary_crossentropy_fn(num_taken, num_skipped=0):
+    return functools.partial(hard_bootstrapping_binary_crossentropy,
+                             num_taken=num_taken,
+                             num_skipped=num_skipped)
